8d0ee0f0872310f861a4924d842a2e9358dd437e
Eduardo Henrique Rigoni <ehrigoni@gmail.com>
2020-05-22 15:56:33 -0300

    VersaoFinaldoDesafio

--- src/App.js
+++ src/App.js
@@ -1,28 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  
+  /* inicialização da variável persistente repositories
+     e de seu método setRepositories
+  */
+  const [repositories, setRepositories] = useState([]);
+  /*
+    Essa função dispara sempre qu ocorrer uma mudança de estado
+  */
+  useEffect(() => {
+    //obtém os diretórios que se encontramna API
+    async function loadData() {
+      const response = await api.get('repositories');
+
+      const loadedRepositories = response.data;
+
+      setRepositories(loadedRepositories);
+    }
+    loadData();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
-  }
+    //recebe o resultado do cadastro de novo projeto na API
+    const newRepository = {
+      title: `New Repository ${Date.now()}`,
+      url: "www.code.com.br",
+      techs: ["NodeJS", "ReactJS"]
+    };
+    
+    const response = await api.post('/repositories', newRepository);
+    
+    const addedRepo = response.data;
+    
+    //se a inserção no back-end foi bem sucedida, então atualiza o front-end
+    if (response.status === 200) {
+          setRepositories([...repositories, addedRepo]);
+    };
+    
+  };
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    
+    //apaga do back-end
+    const response = await api.delete(`/repositories/${id}`); 
 
+    //testa para ver se o repositório retornou sem conteúdo, ou seja, foi apagado
+    if (response.status === 204) {
+      
+      const currentRepositories = repositories.filter(repository => repository.id != id);
+      
+      setRepositories(currentRepositories);
+    }
+    
+  }
+  
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+       <ul data-testid="repository-list">
+         {repositories.map(repository => (
+         
+          <li key={repository.id}>
+               {repository.title}
+               <button onClick={()=>handleRemoveRepository(repository.id)}>Remover</button>
+            </li>
+         ))}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
+        </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
